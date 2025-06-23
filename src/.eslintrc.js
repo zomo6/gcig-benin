d736adf3f123d4eb9a6d9b75ad62a222ca8a6332
zomo6 <geraldosean6@gmail.com>
2025-06-23 15:00:26 +0100

    #comment eslint

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -1,26 +1,26 @@
-   module.exports = {
-     env: {
-       browser: true,
-       es2021: true,
-     },
-     extends: [
-       'eslint:recommended',
-       'plugin:react/recommended',
-       'plugin:@typescript-eslint/recommended',
-     ],
-     parser: '@typescript-eslint/parser',
-     parserOptions: {
-       ecmaFeatures: {
-         jsx: true,
-       },
-       ecmaVersion: 12,
-       sourceType: 'module',
-     },
-     plugins: ['react', '@typescript-eslint'],
-     rules: {
-       'react/no-unescaped-entities': 'off', // Désactiver cette règle
-       '@typescript-eslint/no-unused-vars': 'off', // Désactiver cette règle
-       // Ajoutez d'autres règles que vous souhaitez désactiver
-     },
-   };
+  //  module.exports = {
+  //    env: {
+  //      browser: true,
+  //      es2021: true,
+  //    },
+  //    extends: [
+  //      'eslint:recommended',
+  //      'plugin:react/recommended',
+  //      'plugin:@typescript-eslint/recommended',
+  //    ],
+  //    parser: '@typescript-eslint/parser',
+  //    parserOptions: {
+  //      ecmaFeatures: {
+  //        jsx: true,
+  //      },
+  //      ecmaVersion: 12,
+  //      sourceType: 'module',
+  //    },
+  //    plugins: ['react', '@typescript-eslint'],
+  //    rules: {
+  //      'react/no-unescaped-entities': 'off', // Désactiver cette règle
+  //      '@typescript-eslint/no-unused-vars': 'off', // Désactiver cette règle
+  //      // Ajoutez d'autres règles que vous souhaitez désactiver
+  //    },
+  //  };
    
