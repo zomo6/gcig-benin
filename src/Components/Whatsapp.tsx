fcb896961301005b2b0f8ab6b7f34c307f2f02d8
zomo6 <geraldosean6@gmail.com>
2025-07-09 14:42:30 +0100

    #whatsaap number

--- src/Components/Whatsapp.tsx
+++ src/Components/Whatsapp.tsx
@@ -1,10 +1,12 @@
+"use client"
+
 import { FaWhatsapp } from "react-icons/fa";
 import Head from 'next/head';
 
 function Whatsapp() {
 
    // Numéro de téléphone avec l'indicatif international (sans le +)
-  const phoneNumber = '0152810482'; // Remplacez par votre numéro
+  const phoneNumber = '2290152810482'; // Remplacez par votre numéro
 
    const defaultMessage = encodeURIComponent('Comment puis-je vous aider ?');
   
@@ -19,14 +21,14 @@ function Whatsapp() {
   };
 
   return (
-    <div className="fixed bottom-4 animate-bounce ring-4 right-4 z-50  sm:px-6 sm:py-2 p-2 rounded-full shadow-lg flex bg-green-400 text-White sm:ring-2 ring-white
+    <div onClick={()=>openWhatsApp} className="fixed bottom-4 animate-bounce ring-4 right-4 z-50  sm:px-6 sm:py-2 p-2 rounded-full shadow-lg flex bg-green-400 text-White sm:ring-2 ring-white
      items-center justify-center cursor-pointer hover:bg-[#F39200] transition-colors duration-300 group">
           <Head>
             {/* Meta pour le référencement */}
             <meta name="whatsapp:contact" content={`+${phoneNumber}`} />
           </Head>
 
-        <div className="flex items-center justify-center gap-4" >
+        <div  className="flex items-center justify-center gap-4" >
              <FaWhatsapp className="sm:text-3xl text-4xl font-bold sm:animate-bounce text-white " />
             <p className="sm:flex hidden text-lg font-semibold text-white group-hover:flex">Ecrivez-Nous !</p>
         </div>
