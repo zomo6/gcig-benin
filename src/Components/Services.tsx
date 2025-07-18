237f800cc3b9661edaa8f484cf1880d044547287
zomo6 <geraldosean6@gmail.com>
2025-07-18 14:09:40 +0100

    #netttttttt

--- src/Components/Services.tsx
+++ src/Components/Services.tsx
@@ -307,9 +307,9 @@ const Services: React.FC<ServicesProps> = () => {
                       <Image src="/a.jpg" alt="service de nettoyage et entretien" width={300} height={300}
                        className='w-full h-full object-cover rounded-lg'/>
 
-                      <div className='absolute pt-6  z-40  left-12 text-white items-center justify-center space-y-5 text-center'>
+                      <div className='absolute pt-6  z-40  left-10 text-white items-center justify-center space-y-5 text-center'>
 
-                        <Image src="/nett.jpg" alt="nettoyge et entretien" width={130} height={130}  className='rounded-full p-1 ml-24 ring-4 ring-white'/>
+                        <Image src="/nett.jpg" alt="nettoyge et entretien" width={120} height={120}  className='rounded-full p-1 ml-24 ring-4 ring-white'/>
                 
                            {/* <FaTint className="flex mx-auto items-center text-center p-3 ring-4 ring-white rounded-full text-8xl font-medium"/> */}
 
@@ -332,7 +332,7 @@ const Services: React.FC<ServicesProps> = () => {
                       <Image src="/clef.jpg" alt="service de nettoyage et entretien" width={300} height={300}
                        className='w-full h-full object-cover rounded-lg '/>
 
-                      <div className='absolute   z-40 bottom-14 left-18 text-white items-center justify-center space-y-5 text-center'>
+                      <div className='absolute   z-40 bottom-14 left-16 text-white items-center justify-center space-y-5 text-center'>
 
                 
                            <FaHome className="flex mx-auto items-center text-center p-3 ring-4 ring-white rounded-full text-8xl font-medium"/>
